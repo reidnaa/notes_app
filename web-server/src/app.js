897ceba5a51d33e8d0988f30615c2437342662c8
reid naaykens <reidnaaykens@gmail.com>
2020-09-27 08:07:14 -0700

    added handlebars(hbs) and set up some views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,9 +7,29 @@ const app = express();
 
 const publicDirectoryPath = express.static(path.join(__dirname, '../public'));
 
+app.set('view engine', 'hbs');
+
 app.use(publicDirectoryPath);
 
-
+app.get('', (req, res)=> {
+    res.render('index', {
+        title : "WEATHER TIME",
+        name: "Reid"
+    })
+})
+app.get('/about', (req, res)=> {
+    res.render('about', {
+        title : "About ME",
+        name: "Reid",
+        imgSrc: '/img/image.png'
+    })
+})
+app.get('/help', (req, res)=> {
+    res.render('help', {
+        title : "The Help Page",
+        msg: "Welcome to the help page."
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
